a2f7fdd573d78185b0dbfa73f416096124131fc9
ExtinctionEngine <48667209+ExtinctionEngine@users.noreply.github.com>
2019-08-06 10:23:53 +0930

    regular updates

--- housekeeping.py
+++ housekeeping.py
@@ -9,12 +9,9 @@ def create_data_csv(project_name):
 
 def create_urls_set(url_part_1, n, url_part_2):
     iterator_array_num = arange(n)
-    iterator_array_str = array2string(iterator_array_num)
+    iterator_array_str = iterator_array_num.astype(str)
     url_part_1_array = repeat(url_part_1, n)
     url_part_2_array = repeat(url_part_2, n)
     urls_array = url_part_1_array + iterator_array_str + url_part_2_array
     return urls_array
 
-
-ccc = create_urls_set("aaa", 10, "bbb")
-print(ccc)
