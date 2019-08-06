59aa651c1581fa9d804d7e09422e728fa819ddf5
ExtinctionEngine <qianlima777@gmail.com>
2019-08-06 11:32:52 +0930

    regular updates from PC

--- housekeeping.py
+++ housekeeping.py
@@ -1,4 +1,5 @@
 from numpy import *
+import map
 
 
 def create_data_csv(project_name):
@@ -8,10 +9,15 @@ def create_data_csv(project_name):
 
 
 def create_urls_set(url_part_1, n, url_part_2):
-    iterator_array_num = arange(n)
+    iterator_array_num = set(range(n))
     iterator_array_str = iterator_array_num.astype(str)
     url_part_1_array = repeat(url_part_1, n)
     url_part_2_array = repeat(url_part_2, n)
     urls_array = url_part_1_array + iterator_array_str + url_part_2_array
     return urls_array
+
+
+aaa = range(10)
+print(aaa)
+print(type(aaa))
 
